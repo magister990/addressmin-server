058485808e4b459a253812ec124155abdb41825a
Alex Scott <magister@kfa.cx>
2023-09-07 14:16:52 -0500

    Apply validators to the customer class

--- db/models/customer.py
+++ db/models/customer.py
@@ -1,6 +1,7 @@
 from datetime import datetime
 from sqlalchemy import Column, Boolean, Integer, String, DateTime
 from sqlalchemy.sql import func
+from sqlalchemy.orm import validates
 
 from .base import Base
 
@@ -11,3 +12,7 @@ class Customer(Base):
     name = Column(String, nullable=False)
     created_at = Column(DateTime, server_default=func.now())
     updated_at = Column(DateTime, onupdate=func.now())
+
+    @validates('name')
+    def validate_name(self, field_name, value):
+        return self.validate_unique(field_name, value)
