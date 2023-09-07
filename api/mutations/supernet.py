396cdfda6e32c3e5b8e52ea6b56ede90b9a632bb
Alex Scott <magister@kfa.cx>
2023-09-07 14:23:37 -0500

    Added validation to the supernet and updated the mutations.

--- api/mutations/supernet.py
+++ api/mutations/supernet.py
@@ -7,21 +7,10 @@ from .errors import NotFoundError, AlreadyInUseError
 
 # TODO This one need much more logic around smaller and larger networks
 # overlaping. For now were only doing exact match.
-def is_network_in_use(network: str) -> bool:
-    try:
-        supernet = session.execute(
-            select(Supernet).where(Supernet.network == network)
-        ).scalar_one()
-    except NoResultFound:
-        return False
-    raise NetworkInUseError(f"the network '{network}' is already in use")
-
 # TODO include validation that the customer exists
 
 def resolve_create_supernet(obj, info, network, mask, customer_id):
     try:
-        # First check to see if the name is already in use
-        is_network_in_use(name)
         # Create the new supernet and save.
         supernet = Supernet(network = network, mask = mask, customer_id = customer_id)
         session.add(supernet)
@@ -43,12 +32,9 @@ def resolve_update_supernet(obj, info, id, network, mask, customer_id):
         supernet = session.execute(
             select(Supernet).where(Supernet.id == id)
         ).scalar_one()
-        # Second check if network is already in use
-        is_network_in_use(network)
         # Update the supernet and save.
-        supernet.network = network
-        supernet.mask = mask
-        supernet.customer_id = supernet.customer_id
+        supernet.customer_id = customer_id
+        session.validate_object(supernet)
         session.add(supernet)
         session.commit()
         payload = {
--- db/models/supernet.py
+++ db/models/supernet.py
@@ -3,6 +3,7 @@ from sqlalchemy import Column, Boolean, Integer, String, DateTime
 from sqlalchemy import ForeignKey, ForeignKeyConstraint
 from sqlalchemy.sql import func
 from sqlalchemy.orm import relationship
+from sqlalchemy.orm import validates
 
 from .base import Base
 from .customer import Customer
@@ -27,3 +28,16 @@ class Supernet(Base):
             ondelete="RESTRICT"
         ),
     )
+
+    @validates('network')
+    def validate_network(self, field_name, value):
+        return self.validate_unique(field_name, value)
+
+    @validates('customer_id')
+    def validate_customer_id(self, field_name, value):
+        print(f"validate_customer_id, {self}, {field_name}, {value}")
+        return self.validate_exists(
+            field_name,
+            value,
+            other_class = Customer,
+            other_field = Customer.id)
