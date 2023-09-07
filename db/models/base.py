df84e60f9448f1de1b34ba66a03bb2fbd50a1a32
Alex Scott <magister@kfa.cx>
2023-09-07 14:16:13 -0500

    Created a universal validators for checking uniqueness and for checking existance.

--- db/models/base.py
+++ db/models/base.py
@@ -1,5 +1,6 @@
-#from functools import classmethod
 from sqlalchemy.ext.declarative import as_declarative, declared_attr
+from sqlalchemy import select
+from db.connection import session
 
 @as_declarative()
 class BaseBase:
@@ -22,3 +23,35 @@ class Base(BaseBase):
         for col in columns:
             result[col]=getattr(self, col)
         return result
+
+    def validate_unique(
+        self,
+        field_name,
+        value,
+        message = "The '{field}' of '{value}' is aready in use."
+    ):
+        if not value:
+            return value
+        query = select(self.__class__).where(getattr(self.__class__, field_name) == value)
+        result = session.execute(query).scalars().first()
+        if result:
+            raise ValueError(message.format(field = field_name, value = value))
+        return value
+
+    def validate_exists(
+        self,
+        field_name,
+        value,
+        message = "The '{field}' of '{value}' does not exist.",
+        other_class = None,
+        other_field = None
+    ):
+        if not value:
+            return value
+        select_class = other_class if other_class else self.__class__
+        select_field = other_field if other_field else getattr(self.__class__, field_name)
+        query = select(select_class).where(select_field == value)
+        result = session.execute(query).scalars().first()
+        if not result:
+            raise ValueError(message.format(field = field_name, value = value))
+        return value
